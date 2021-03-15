f289d2c3c5575a647d5e1efa370788fef9c0dded
neha racharla <nracharla@avaya.com>
2021-03-15 15:24:23 -0500

    view menu added

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date('March 5, 2021 12:00:00').getTime();
+var countDownDate = new Date('March 17, 2021 11:30:00').getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
